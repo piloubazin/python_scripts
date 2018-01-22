3f715a9f516263c599219115f740de127ec1fb1d
Pilou Bazin <piloubazin@gmail.com>
2018-01-22 10:55:29 +0100

    optimized speed for isc code

--- inter_subject_correlation.py
+++ inter_subject_correlation.py
@@ -127,22 +127,25 @@ def inter_subject_correlation(fmri_files, mask_files, transform_files, outdir, n
     
     # export results (resampled to avg space)
     transform = nb.load(transform_files[0])
-    nx = transform.header.get_data_shape()[X]
-    ny = transform.header.get_data_shape()[Y]
-    nz = transform.header.get_data_shape()[Z]
+    nx = transform.shape[X]
+    ny = transform.shape[Y]
+    nz = transform.shape[Z]
     resampled = np.zeros((nx,ny,nz,nsubjects))
-    for x in range(p0[X],pM[X]):
-        for y in range(p0[Y],pM[Y]):
-            for z in range(p0[Z],pM[Z]):
-                if (avgmask[x,y,z]>0) :
-                    for s in range(nsubjects):
-                        resampled[x,y,z,s] = correlation[x-p0[X],y-p0[Y],z-p0[Z],s]
+    resampled[p0[X]:pM[X],p0[Y]:pM[Y],p0[Z]:pM[Z],:] = correlation
+#    for x in range(p0[X],pM[X]):
+#        for y in range(p0[Y],pM[Y]):
+#            for z in range(p0[Z],pM[Z]):
+#                if (avgmask[x,y,z]>0) :
+#                    for s in range(nsubjects):
+#                        resampled[x,y,z,s] = correlation[x-p0[X],y-p0[Y],z-p0[Z],s]
                         
     outname = outdir+basename[0]+"_isc"+str(nsubjects)+"x"+str(nruns)+"_corr."+".".join(basename[1:])
     print("saving to: "+outname)
-    outimg = nb.Nifti1Image(resampled, transform.affine, transform.header)
-    outimg.header['cal_min'] = np.min(resampled)
-    outimg.header['cal_max'] = np.max(resampled)
+    header = transform.get_header()
+    affine = transform.get_affine()
+    header['cal_min'] = np.min(resampled)
+    header['cal_max'] = np.max(resampled)
+    outimg = nb.Nifti1Image(resampled, affine, header)
     outimg.to_filename(outname)
     
     # 3. additional statistics?
@@ -174,10 +177,10 @@ def compute_subject_correlations(fmri_files, mask_files, transform_files, outdir
 
         # keep sums in case of multiple runs per subject
         
-        covar = np.zeros((pM[X]-p0[X],pM[Y]-p0[Y],pM[Z]-p0[Z]))
-        var_sub = np.zeros((pM[X]-p0[X],pM[Y]-p0[Y],pM[Z]-p0[Z]))
-        var_avg = np.zeros((pM[X]-p0[X],pM[Y]-p0[Y],pM[Z]-p0[Z]))
-        samples = np.zeros((pM[X]-p0[X],pM[Y]-p0[Y],pM[Z]-p0[Z]))
+        covar = np.zeros((pM[X]-p0[X],pM[Y]-p0[Y],pM[Z]-p0[Z]), dtype=np.float64)
+        var_sub = np.zeros((pM[X]-p0[X],pM[Y]-p0[Y],pM[Z]-p0[Z]), dtype=np.float64)
+        var_avg = np.zeros((pM[X]-p0[X],pM[Y]-p0[Y],pM[Z]-p0[Z]), dtype=np.float64)
+        samples = np.zeros((pM[X]-p0[X],pM[Y]-p0[Y],pM[Z]-p0[Z]), dtype=np.float64)
 
         for r in range(nruns) :
             if debug : print("run: "+str(r+1))    
@@ -199,38 +202,55 @@ def compute_subject_correlations(fmri_files, mask_files, transform_files, outdir
                 fmri = build_zscore(fmri, mask, transform, skip_first, skip_last, meancutoff)
                 
                 # remove from global average
-                for x in range(p0[X],pM[X]):
-                    for y in range(p0[Y],pM[Y]):
-                        for z in range(p0[Z],pM[Z]):
-                            if (avgmask[x,y,z]>0) :
-                                xp = int(np.rint(transform[x,y,z,X]))
-                                yp = int(np.rint(transform[x,y,z,Y]))
-                                zp = int(np.rint(transform[x,y,z,Z]))
-                            
-                                if (mask[xp,yp,zp]>0) : # only where you have valid data
-                                    for t in range(skip_first, fmri.shape[T]-skip_last) :
-                                        # remove normalization
-                                        avgdata = avgfmri[x-p0[X],y-p0[Y],z-p0[Z],runtimes[r]+t-skip_first] \
-                                                 *avgcount[x-p0[X],y-p0[Y],z-p0[Z],r]
-                                        # remove data value
-                                        avgdata -= fmri[xp,yp,zp,t]
-                                        # re-normalize
-                                        avgdata /= (avgcount[x-p0[X],y-p0[Y],z-p0[Z],r]-1)
-                                        # update variances and covariance
-                                        covar[x-p0[X],y-p0[Y],z-p0[Z]] += fmri[xp,yp,zp,t]*avgdata
-                                        var_sub[x-p0[X],y-p0[Y],z-p0[Z]] += fmri[xp,yp,zp,t]*fmri[xp,yp,zp,t]
-                                        var_avg[x-p0[X],y-p0[Y],z-p0[Z]] += avgdata*avgdata
-                                        samples[x-p0[X],y-p0[Y],z-p0[Z]] += 1
+#                for x in range(p0[X],pM[X]):
+#                    for y in range(p0[Y],pM[Y]):
+#                        for z in range(p0[Z],pM[Z]):
+#                            if (avgmask[x,y,z]>0) :
+#                                xp = int(np.rint(transform[x,y,z,X]))
+#                                yp = int(np.rint(transform[x,y,z,Y]))
+#                                zp = int(np.rint(transform[x,y,z,Z]))
+#                            
+#                                if (mask[xp,yp,zp]>0) : # only where you have valid data
+#                                   for t in range(skip_first, fmri.shape[T]-skip_last) :
+#                                       # remove normalization
+#                                        avgdata = avgfmri[x-p0[X],y-p0[Y],z-p0[Z],runtimes[r]+t-skip_first] \
+#                                                 *avgcount[x-p0[X],y-p0[Y],z-p0[Z],r]
+#                                        # remove data value
+#                                        avgdata -= fmri[xp,yp,zp,t]
+#                                        # re-normalize
+#                                        avgdata /= (avgcount[x-p0[X],y-p0[Y],z-p0[Z],r]-1)
+#                                        # update variances and covariance
+#                                        covar[x-p0[X],y-p0[Y],z-p0[Z]] += fmri[xp,yp,zp,t]*avgdata
+#                                        var_sub[x-p0[X],y-p0[Y],z-p0[Z]] += fmri[xp,yp,zp,t]*fmri[xp,yp,zp,t]
+#                                        var_avg[x-p0[X],y-p0[Y],z-p0[Z]] += avgdata*avgdata
+#                                        samples[x-p0[X],y-p0[Y],z-p0[Z]] += 1
+
+                # remove from global average
+                transform = np.rint(transform).astype(int)
+                cropped = transform[p0[X]:pM[X],p0[Y]:pM[Y],p0[Z]:pM[Z],:]
+                # remove current subject from average
+                avgdata = avgfmri[:,:,:,runtimes[r]:runtimes[r+1]]*avgcount[:,:,:,r,np.newaxis]
+                avgdata -= fmri[cropped[:,:,:,X],cropped[:,:,:,Y],cropped[:,:,:,Z],skip_first:fmri.shape[T]-skip_last]
+                avgdata /= np.fmax(avgcount[:,:,:,r,np.newaxis]-1,np.ones(avgdata.shape))
+                # update variance and covariance
+                for t in range(skip_first, fmri.shape[T]-skip_last) :
+                    covar += np.multiply(fmri[cropped[:,:,:,X],cropped[:,:,:,Y],cropped[:,:,:,Z],t],avgdata[:,:,:,t-skip_first])
+                    var_sub += np.multiply(fmri[cropped[:,:,:,X],cropped[:,:,:,Y],cropped[:,:,:,Z],t],fmri[cropped[:,:,:,X],cropped[:,:,:,Y],cropped[:,:,:,Z],t])
+                    var_avg += np.multiply(avgdata[:,:,:,t-skip_first],avgdata[:,:,:,t-skip_first])    
+                    samples += 1
 
         # compute the final correlations
-        for x in range(p0[X],pM[X]):
-            for y in range(p0[Y],pM[Y]):
-                for z in range(p0[Z],pM[Z]):
-                    if (avgmask[x,y,z]>0) :
-                        correlation[x-p0[X],y-p0[Y],z-p0[Z],s] = covar[x-p0[X],y-p0[Y],z-p0[Z]] \
-                                                                /np.sqrt(var_sub[x-p0[X],y-p0[Y],z-p0[Z]]) \
-                                                                /np.sqrt(var_avg[x-p0[X],y-p0[Y],z-p0[Z]])
-                                               
+#        for x in range(p0[X],pM[X]):
+#            for y in range(p0[Y],pM[Y]):
+#                for z in range(p0[Z],pM[Z]):
+#                    if (avgmask[x,y,z]>0) :
+#                        correlation[x-p0[X],y-p0[Y],z-p0[Z],s] = covar[x-p0[X],y-p0[Y],z-p0[Z]] \
+#                                                                /np.sqrt(var_sub[x-p0[X],y-p0[Y],z-p0[Z]]) \
+#                                                                /np.sqrt(var_avg[x-p0[X],y-p0[Y],z-p0[Z]])
+ 
+        # compute the final correlations
+        correlation[:,:,:,s] = covar/(np.sqrt(np.multiply(var_sub,var_avg)))
+ 
     return correlation                       
 
 
@@ -335,30 +355,39 @@ def build_average( fmri_files, mask_files, transform_files, outdir,
 						
         # build the global average: array version?
         transform = np.rint(transform).astype(int)
-        cropped = transform-p0[np.newaxis,np.newaxis,np.newaxis,:]
-        np.clip(cropped[:,:,:,X], 0, pM[X]-p0[X]-1, out=cropped[:,:,:,X])
-        np.clip(cropped[:,:,:,Y], 0, pM[Y]-p0[Y]-1, out=cropped[:,:,:,Y])
-        np.clip(cropped[:,:,:,Z], 0, pM[Z]-p0[Z]-1, out=cropped[:,:,:,Z])
-       
-        avgfmri[cropped[:,:,:,X],cropped[:,:,:,Y],cropped[:,:,:,Z],runtimes[r-1]:runtimes[r]] \
-                += (mask[transform[:,:,:,X],transform[:,:,:,Y],transform[:,:,:,Z]]>0) \
-                *fmri[transform[:,:,:,X],transform[:,:,:,Y],transform[:,:,:,Z],skip_first:fmri.shape[T]-skip_last]
+        cropped = transform[p0[X]:pM[X],p0[Y]:pM[Y],p0[Z]:pM[Z],:]
+        #np.clip(cropped[:,:,:,X], 0, pM[X]-p0[X]-1, out=cropped[:,:,:,X])
+        #np.clip(cropped[:,:,:,Y], 0, pM[Y]-p0[Y]-1, out=cropped[:,:,:,Y])
+        #np.clip(cropped[:,:,:,Z], 0, pM[Z]-p0[Z]-1, out=cropped[:,:,:,Z])
+        
+        avgfmri[:,:,:,runtimes[r-1]:runtimes[r]] \
+                += (mask[cropped[:,:,:,X],cropped[:,:,:,Y],cropped[:,:,:,Z],np.newaxis]>0) \
+                *fmri[cropped[:,:,:,X],cropped[:,:,:,Y],cropped[:,:,:,Z],skip_first:fmri.shape[T]-skip_last]
                 
-        avgcount[cropped[:,:,:,X],cropped[:,:,:,Y],cropped[:,:,:,Z],r-1] \
-                += (mask[transform[:,:,:,X],transform[:,:,:,Y],transform[:,:,:,Z]]>0)
+        avgcount[:,:,:,r-1] \
+                += (mask[cropped[:,:,:,X],cropped[:,:,:,Y],cropped[:,:,:,Z]]>0)
 						
     # final step: average, discard data with unsufficient number of subjects
-    for x in range(p0[X],pM[X]):
-        for y in range(p0[Y],pM[Y]):
-            for z in range(p0[Z],pM[Z]):
-                for r in range(nruns) :
-                    if (avgcount[x-p0[X],y-p0[Y],z-p0[Z],r]<=groupcutoff*nsubjects) :
-                        avgmask[x,y,z] = 0
-                
-                if (avgmask[x,y,z]>0) :
-                    for r in range(nruns) :
-                        for t in range(runtimes[r], runtimes[r+1]) :
-                            avgfmri[x-p0[X],y-p0[Y],z-p0[Z],t] /= avgcount[x-p0[X],y-p0[Y],z-p0[Z],r]
+#    for x in range(p0[X],pM[X]):
+#        for y in range(p0[Y],pM[Y]):
+#            for z in range(p0[Z],pM[Z]):
+#                for r in range(nruns) :
+#                    if (avgcount[x-p0[X],y-p0[Y],z-p0[Z],r]<=groupcutoff*nsubjects) :
+#                        avgmask[x,y,z] = 0
+#                
+#                if (avgmask[x,y,z]>0) :
+#                    for r in range(nruns) :
+#                        for t in range(runtimes[r], runtimes[r+1]) :
+#                            avgfmri[x-p0[X],y-p0[Y],z-p0[Z],t] /= avgcount[x-p0[X],y-p0[Y],z-p0[Z],r]
+
+    # final step: average, discard data with unsufficient number of subjects
+    for r in range(nruns) :
+        avgmask[p0[X]:pM[X],p0[Y]:pM[Y],p0[Z]:pM[Z]] *= (avgcount[:,:,:,r]>groupcutoff*nsubjects)
+
+    np.fmax(avgcount,np.ones(avgcount.shape), out=avgcount)
+    for r in range(nruns) :
+        avgfmri[:,:,:,runtimes[r]:runtimes[r+1]] /= avgcount[:,:,:,r,np.newaxis]
+
     if saveavgfmri :
         imgname = os.path.basename(fmri_files[0])
         basename = imgname.split(".")
