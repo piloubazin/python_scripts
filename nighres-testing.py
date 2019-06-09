382c6a64ce8b1d66669928c795fd9679e01f5b9c
piloubazin <piloubazin@gmail.com>
2019-06-09 08:36:03 +0200

    testing the idea to build streamlines from ventricles to pial surface

--- nighres-testing.py
+++ nighres-testing.py
@@ -23,7 +23,11 @@ if len(imgfiles)==2:
 	#				run_affine_first=True, cost_function='CrossCorrelation', interpolation='NearestNeighbor',
     #                save_data=True, output_dir=outdir,
     #                file_name=None)
-	nh.registration.embedded_syn(source_image=imgfiles[0], target_image=imgfiles[1], coarse_iterations=20, medium_iterations=0, fine_iterations=0)
+	#nh.registration.embedded_syn(source_image=imgfiles[0], target_image=imgfiles[1], coarse_iterations=20, medium_iterations=0, fine_iterations=0)
+	nh.laminar.laminar_iterative_smoothing(profile_surface_image=imgfiles[0], intensity_image=imgfiles[1], fwhm_mm=5,
+                     save_data=True, output_dir=outdir,
+                     file_name=None)
+
 
 if len(imgfiles)==4:
     nh.cortex.cruise_cortex_extraction(init_image=imgfiles[0], wm_image=imgfiles[1], gm_image=imgfiles[2], csf_image=imgfiles[3], vd_image=None,
